ea13516e331a3f2927a28fcf91398af88951e66d
pengfuyuzhen <pfyz2011@gmail.com>
2018-04-27 15:43:08 -0400

    Initialize project

---
+++ Lively/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lively
+//
+//  Created by Fuyuzhen Peng on 4/27/18.
+//  Copyright © 2018 Fuyuzhen Peng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

b58e6c6b181e267ab393ef3b06cd15ac3e9e6a78
pengfuyuzhen <pfyz2011@gmail.com>
2018-04-30 19:39:44 -0400

    Added UI for choose event screen

---
+++ Lively/View Controllers/ChooseEventViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+final class ChooseEventViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func cancel(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 8
+    }
+}
